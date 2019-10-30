9d65a3f08c41b4dc2f657a2f8f5a53369e4fae3b
Ary AS <aryadhigana>
2019-10-30 17:49:13 +0700

    first commit

---
+++ src/kelasSatu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author artz_
+ */
+public class kelasSatu {
+    public static void main(String[] args) {
+        
+    }
+    private static void loadDataTrain(int column, int row){
+        int[][] dataTrain = new int [column][row];
+        
+        
+        
+    }
+}
---
+++ src/ta_ary/TA_Ary.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ta_ary;
+
+/**
+ *
+ * @author artz_
+ */
+public class TA_Ary {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
